0b4f02a9d3db1921817be2da735a6c9d3d798290
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-22 18:07:20 -0500

    Conexión usuarios

--- Conexión.php
+++ Conexión.php
@@ -1,12 +1,12 @@
 <?php
     $servername = "localhost";
-    $database = $dbname = "life";
-    $username = "root";
-    $password = "";
+    $database = $dbname = "usuarios";
+    $username = "dantecc10";
+    $password = "@dantecc10!";
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $database);
     // Check connection
     if (!$conn) {
         die("<p>Error al conectar con la base de datos: " . mysqli_connect_error() . "</p><p>Pruebe a verificar la conexión del servidor...</p>");
     }
-    echo "<script>console.log('Conexión a la base de datos: Exitosa')</script>";
+    echo "Conexión a la base de datos: Exitosa";
---
+++ InicioSesión.php
@@ -0,0 +1,2 @@
+<?php
+include("ConexiónUsuarios.php");
