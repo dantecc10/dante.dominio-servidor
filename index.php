1e632211f1b54eb73c35fbc98d01cb63707e2307
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-24 10:27:27 -0500

    CSS JS

--- index.php
+++ index.php
@@ -2,13 +2,41 @@
 <html lang="es">
 
 <head>
+    <link rel="shortcut icon" href="../IMG/FAVICON TRANSPARENTE.png" type="image/x-icon">
+    <link id="EtiquetaCSS" rel="Stylesheet" href="../CSS/CSS GLOBAL - TEMA OSCURO.css" Type="text/css" MEDIA="screen">
+    <link id="Oculta/Muestra" rel="Stylesheet" href="../CSS/CSS OCULTAMIENTO.css" Type="text/css" MEDIA="screen">
+
+    <meta name="description" content="Dante Castelán Carpinteyro: Web dedicada a publicación de proyectos y código personal.">
+    <meta name="author" content="Dante Castelán Carpinteyro">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Dante - Inicio</title>
 </head>
 
 <body>
+    <script src="../JavaScript/CambiarTema.js"></script>
+    <!--Encabezado (no mover)--> <br>
+    <header id="Encabezado">
+        <div id="Contenedor-Encabezado">
+            <div id="Contenido-Encabezado">
+                <h1>CC.club</h1> <a href="index.php"> <img id="Logo" src="../IMG/FAVICON.PNG" alt="dante.castelancarpinteyro.club">
+                </a>
+                <div class="Menú-Desplegable"> <button class="Botón-Desplegar">Menú</button>
+                    <div class="Contenido-Desplegable">
+                        <a href="IniciarSesión.php">IniciarSesión</a>
+                        <a href="">POLITIZA MX</a>
+                        <a href="AcercaDe.php">Acerca de...</a>
+                        <a id="CambiaTema" onclick="javascript:CambiarTema();">Cambiar tema</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="Encabezado-Superior"></div>
+        <div id="Encabezado-Inferior"></div>
+    </header>
+
+
     <h1>Inicio</h1>
     <h2>En construcción...</h2>
     <p>Pronto actualizaré el código de index... paciencia</p>
