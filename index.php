6fe4f41738c899165a0ddee7779f9bd5d959d2d3
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-24 10:14:04 -0500

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <h1>Inicio</h1>
+    <h2>En construcción...</h2>
+    <p>Pronto actualizaré el código de index... paciencia</p>
+    <p align="left">- Dante Castelán Carpinteyro</p>
+</body>
+
+</html>
